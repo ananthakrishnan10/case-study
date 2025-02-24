47d85672620f7c2ac81442c4db95c72f384842df
Ananthan <krishnan95.ak@gmail.com>
2025-02-24 22:14:05 +0530

    feat: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
-
   return (
-    <><h1 className="text-3xl font-bold underline">case study</h1></>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">case study</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
